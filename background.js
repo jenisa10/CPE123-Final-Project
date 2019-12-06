1259cc2783629270e966a9ff935ff85213bd9a9a
konanyuta <58442094+konanyuta@users.noreply.github.com>
2019-12-05 19:37:53 -0800

    Update background.js

--- background.js
+++ background.js
@@ -729,3 +729,83 @@ function racket() {
 	fill(255);
 	ellipse(0, 0, 20);
  }
+
+
+//for the fireworks @ end screen
+// end firework vars
+var end_fireworks = [];
+var col = 1;
+function end_Particle(x , y) 
+{
+   this.accelY = 0.05; //gravity
+   this.velX = random(-1, 1);
+   this.velY = random(.5, 1.3);
+
+   if(col % 3 < 1){
+      this.pcolorR=255
+      this.pcolorG=166+random(-50,50)
+      this.pcolorB=247+random(-50,50)
+   }
+   else if(col % 3 < 2){
+      this.pcolorR=145+random(-50,50)
+      this.pcolorG=255
+      this.pcolorB=176+random(-50,50)
+   }
+   else if(col % 3 < 3){
+      this.pcolorR=145+random(-50,50)
+      this.pcolorG=255+random(-50,50)
+      this.pcolorB=255
+   }
+   this.locX = x;
+   this.locY = y;
+   this.r = 8.0;
+   this.life = 255;
+  
+   this.updateP = function()
+   {
+      this.velY += this.accelY;
+      this.locX += this.velX;
+      this.locY += this.velY;
+      this.life -= 2.5;
+   };
+  
+   this.renderP = function() 
+   {
+      noStroke();
+      push();
+         fill(this.pcolorR, this.pcolorG, this.pcolorB, this.life);
+         translate(this.locX, this.locY);
+         ellipse(0, 0, this.r, this.r);
+      pop();
+   };
+} //end of particle object definition
+
+function end_PSys(sX, sY, num)
+{
+   this.particles = [];
+   for (var i=0; i < num; i++) 
+   {
+      this.particles.push(new end_Particle(sX, sY));
+   }
+  
+   this.run = function() 
+   {
+      for (var i=0; i < this.particles.length; i++) 
+      {
+         //update each particle per frame
+         this.particles[i].updateP();
+         this.particles[i].renderP();
+      }
+   }
+}
+
+function fireworkdraw(){ //runs the particle system
+	 for(i=0;i<fireworks.length;i++){
+      fireworks[i].run()
+   }
+}
+
+function endFireworks(){ //creates the fireworks
+   fireworks.push(new PSys(random(width), random(height), random(15, 25)));
+   col++
+}
